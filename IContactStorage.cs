4aff91e9f02604bb59995949dfdf8d09dfe86d65
olcia2098 <144455848+olcia2098@users.noreply.github.com>
2024-02-19 13:10:04 +0100

    Icontactdone

--- IContactStorage.cs
+++ IContactStorage.cs
@@ -2,6 +2,8 @@ namespace ContactManagementSystem
 {
     public interface IContactStorage
     {
+        List<Contact> LoadContacts();
 
+        void SaveContacts(List<ContactManagementSystem>contacts);
     }
 }
