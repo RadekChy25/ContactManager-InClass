f8bcd784bfe423c06699efb3ef4461e78ad48785
DomJab23 <dojab23@studnet.sdu.dk>
2024-02-19 13:39:07 +0100

    Changes

--- FileContactStorage.cs
+++ FileContactStorage.cs
@@ -10,16 +10,30 @@ namespace ContactManagementSystem
         private readonly string filePath = "contacts.json";
         public List<Contact> LoadContacts()
         {
+            if(FileContactStorage.Exists(filePath))
+            {
+                string json = FileContactStorage.ReadAllText(filePath);
+                List<ContactManagementSystem> contacts = JsonSerializer.Deserialize<List<ContactManagementSystem>>(json);
+
+                    return contacts;
+            }
+            else
+            {
+                return new List<ContactManagementSystem>();
+            }
             // Load contacts from JSON file if exists, else return new list
             // Use JsonSerializer.Deserialize to convert JSON string to List<Contact>
-            throw new NotImplementedException();
         }
 
         public void SaveContacts(List<Contact> contacts)
         {
+            var options = new JsonSerializerOptions { WriteIndented = true};
+
+            string json = JsonSerializer.Serializer(contacts, options);
+
+            FileContactStorage.WriteAllText(filePath, json);
             // Save contacts to JSON file
             // Use JsonSerializer.Serialize with WriteIndented option for pretty print
-            throw new NotImplementedException();
         }
     }
 }
