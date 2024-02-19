07e1c195d41cc02998192f5bb79fd32ef2955abe
Enrique Suarez <esuarezgrandal@gmail.com>
2024-02-19 13:22:26 +0100

    hey

--- Contact.cs
+++ Contact.cs
@@ -2,6 +2,19 @@ namespace ContactManagementSystem
 {
     public class Contact
     {
+        public int Id{ get; set; };
+        public string Name{ get; set; };
+        public string Email{ get; set; };
+        public long int PhoneNumber{ get; set; };
+
+        public Contact(int id, string name, string email, long int phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
 
     }
 }
